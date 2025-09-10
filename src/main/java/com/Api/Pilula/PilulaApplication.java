fd2ee05b6983cb51a6b2a952a700a95dd2d4858f
ed <eduardodsxavier@gmail.com>
2025-09-10 18:53:34 -0300

    enviroment setup

---
+++ src/main/java/com/Api/Pilula/PilulaApplication.java
@@ -0,0 +1,13 @@
+package com.Api.Pilula;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PilulaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PilulaApplication.class, args);
+	}
+
+}
