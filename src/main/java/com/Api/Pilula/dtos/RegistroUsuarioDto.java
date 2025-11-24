8f0f37e99b8b656a08d7e946164e48bac847b34c
= <=>
2025-11-23 23:13:37 -0300

    pequeno tratamento dos erros e não permite envio de cpf com menos de 11 caracteres

--- src/main/java/com/Api/Pilula/dtos/RegistroUsuarioDto.java
+++ src/main/java/com/Api/Pilula/dtos/RegistroUsuarioDto.java
@@ -1,8 +1,10 @@
 package com.Api.Pilula.dtos;
 
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
 
 public record RegistroUsuarioDto(
+    @Pattern(regexp = "\\d{11}", message = "CPF deve conter exatamente 11 dígitos numéricos")
     String cpf,
     String nome,
     @NotBlank String email,
